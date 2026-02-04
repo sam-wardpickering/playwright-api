f941f90cec4d357433b3248eecb3cffead00fbc0
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-02-04 16:12:58 +1300

    Refactor test

--- tests/envTest.spec.js
+++ tests/envTest.spec.js
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('Testing dotenv works', async ({ page }) => {
     await page.goto('/');
 
-    expect(page).toHaveURL(/the-internet/);
+    await expect(page).toHaveURL(/the-internet/);
 });
