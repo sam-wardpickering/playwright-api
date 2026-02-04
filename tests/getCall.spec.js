acdad18357006597c53e97669d561de3a7cbe870
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-02-04 13:09:43 +1300

    Add get request, capture response

--- tests/getCall.spec.js
+++ tests/getCall.spec.js
@@ -2,4 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('Test GET API', async ({ request }) => {
 
+    const response = await request.get("https://jsonplaceholder.typicode.com/posts/1");
+
+    console.log(response);
 });
