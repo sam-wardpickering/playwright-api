d9ecc929dea5ab9a80d9d3fed387ab4a18e16156
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-02-05 16:14:04 +1300

    Create new test fiule for post calls

---
+++ tests/postCall.spec.js
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('Post call example with token and booking ID', async ({ request }) => {
+    const authData = {
+        "username" : "admin",
+        "password" : "password123"
+    }
+
+    const response = await request.post('https://restful-booker.herokuapp.com/auth', {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        data: authData
+    })
+
+    console.log(response.status());
+    
+});
