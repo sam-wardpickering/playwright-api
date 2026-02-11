a9260cea98d6c913a8a71488a0dc2026b9e26dd0
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-02-11 18:10:28 +1300

    Console log ping status

--- tests/monitorApi.spec.js
+++ tests/monitorApi.spec.js
@@ -2,4 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test('Check API Health', async ({ request }) => {
     const pingResponse = await request.get("https://restful-booker.herokuapp.com/ping");
+
+    const pingStatus = pingResponse.status();
+
+    console.log(`Response code from API is ${pingStatus}`);
 });
