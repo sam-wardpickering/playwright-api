5a30a3eac5ed27b977fb0e8183ad157a88455058
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-02-11 18:23:12 +1300

    Update api monitor to check status and response duration

--- tests/monitorApi.spec.js
+++ tests/monitorApi.spec.js
@@ -1,9 +1,34 @@
 import { test, expect } from '@playwright/test';
 
+// Playwright timeout is 30 seconds by default
+
 test('Check API Health', async ({ request }) => {
-    const pingResponse = await request.get("https://restful-booker.herokuapp.com/ping");
 
-    const pingStatus = pingResponse.status();
+    test.setTimeout(0);
 
-    console.log(`Response code from API is ${pingStatus}`);
+    while(true) {
+
+        const startTime = Date.now(); 
+
+        const pingResponse = await request.get("https://restful-booker.herokuapp.com/ping");
+
+        const endTime = Date.now();
+
+        const responseDuration = endTime-startTime;
+        
+        
+
+        if (responseDuration > 2000) {
+            throw new Error(`API response is slow ${duration}`);
+        } else {
+            console.log(`Total duration of the response is ${responseDuration}`);
+        }
+        
+        const pingStatus = pingResponse.status();
+
+        console.log(`Response code from API is ${pingStatus}`);
+
+        expect(pingStatus).toBe(201);
+    }
+
 });
