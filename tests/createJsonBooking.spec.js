9f4049cbb2e5752d32e7b98a0f47d54c496d2ab3
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-02-12 17:30:27 +1300

    Set up initial test

--- tests/createJsonBooking.spec.js
+++ tests/createJsonBooking.spec.js
@@ -1,5 +1,12 @@
 import { test, expect } from '@playwright/test';
 
+const bookingBaseUrl = process.env.BOOKING_API_BASE_URL;
+
 test('Create New Booking With Post Call', async ({ request }) => {
-    // code here
+    await request.post(`${bookingBaseUrl}/booking`, {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        // data:  
+    });
 });
