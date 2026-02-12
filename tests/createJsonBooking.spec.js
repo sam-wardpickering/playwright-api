f7a56f3a30768034b13208fcc41255395068920a
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-02-12 17:35:48 +1300

    Import fs

--- tests/createJsonBooking.spec.js
+++ tests/createJsonBooking.spec.js
@@ -1,8 +1,12 @@
 import { test, expect } from '@playwright/test';
+import fs from 'fs';
 
 const bookingBaseUrl = process.env.BOOKING_API_BASE_URL;
 
 test('Create New Booking With Post Call', async ({ request }) => {
+
+    const bookingFile = fs.readFileSync("./testData/booking.json");
+
     await request.post(`${bookingBaseUrl}/booking`, {
         headers: {
             "Content-Type": "application/json"
